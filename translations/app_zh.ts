21a1b94b32995c56a34bcfcb1a65fddb601da930
SeptemberHX <september_hx@outlook.com>
2020-05-16 12:40:43 +0800

    add: chinese translation

---
+++ translations/app_zh.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>PstateMainWidget</name>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="23"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="440"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="440"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="48"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="441"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="441"/>
+        <source>Processor Settings</source>
+        <translation>处理器设置</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="72"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="442"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="442"/>
+        <source>CPU Frequencies</source>
+        <translation>CPU频率</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="120"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="443"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="443"/>
+        <source>Min perf</source>
+        <translation>最小</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="155"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="444"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="444"/>
+        <source>19%</source>
+        <translation>19%</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="195"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="445"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="445"/>
+        <source>Max perf</source>
+        <translation>最大</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="230"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="446"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="446"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="270"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="447"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="447"/>
+        <source>Turbo</source>
+        <translation>Turbo</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="293"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="449"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="449"/>
+        <source>GPU Frequencies</source>
+        <translation>显卡频率</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="341"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="450"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="450"/>
+        <source>Min freq</source>
+        <translation>最小</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="370"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="451"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="451"/>
+        <source>300 MHz</source>
+        <translation>300 MHz</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="410"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="452"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="452"/>
+        <source>Max freq</source>
+        <translation>最大</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="439"/>
+        <location filename="../PstateMainWidget.ui" line="508"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="453"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="455"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="453"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="455"/>
+        <source>1150 MHz</source>
+        <translation>1150 MHz</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="479"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="454"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="454"/>
+        <source>Boost freq</source>
+        <translation>Boost 频率</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="524"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="456"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="456"/>
+        <source>CPU Governor</source>
+        <translation>CPU模式</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="533"/>
+        <location filename="../PstateMainWidget.ui" line="539"/>
+        <location filename="../PstateMainWidget.ui" line="542"/>
+        <location filename="../PstateMainWidget.ui" line="548"/>
+        <location filename="../PstateMainWidget.ui" line="707"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="458"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="461"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="464"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="466"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="489"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="458"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="461"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="464"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="466"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="489"/>
+        <source>Performance</source>
+        <translation>性能</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="571"/>
+        <location filename="../PstateMainWidget.ui" line="574"/>
+        <location filename="../PstateMainWidget.ui" line="577"/>
+        <location filename="../PstateMainWidget.ui" line="580"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="468"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="471"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="474"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="476"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="468"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="471"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="474"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="476"/>
+        <source>Ondemand</source>
+        <translation>按需</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="600"/>
+        <location filename="../PstateMainWidget.ui" line="603"/>
+        <location filename="../PstateMainWidget.ui" line="606"/>
+        <location filename="../PstateMainWidget.ui" line="609"/>
+        <location filename="../PstateMainWidget.ui" line="772"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="478"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="481"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="484"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="486"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="492"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="478"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="481"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="484"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="486"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="492"/>
+        <source>Powersave</source>
+        <translation>省电</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="660"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="487"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="487"/>
+        <source>Energy Performance</source>
+        <translation>电源性能</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="684"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="488"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="488"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="732"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="490"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="490"/>
+        <source>Balance Performance</source>
+        <translation>平衡-性能</translation>
+    </message>
+    <message>
+        <location filename="../PstateMainWidget.ui" line="752"/>
+        <location filename="../build/ui_PstateMainWidget.h" line="491"/>
+        <location filename="../cmake-build-debug/ui_PstateMainWidget.h" line="491"/>
+        <source>Balance Powersave</source>
+        <translation>平衡-省电</translation>
+    </message>
+</context>
+</TS>
